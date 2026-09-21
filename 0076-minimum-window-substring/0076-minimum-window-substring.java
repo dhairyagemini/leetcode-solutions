1614c205013cf0583b5a464b64ba00cf7b6e17a7
Dhairya Gemini <275282816+dhairyagemini@users.noreply.github.com>
2026-09-21 10:49:04 +0530

    Time: 21 ms (30.63%), Space: 48 MB (13.88%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,42 +1,47 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(s.length()==0||t.length()==0||s.length()<t.length())return "";
-        Map<Character,Integer>map=new HashMap<>();
-        for(int i=0;i<t.length();i++){
-            map.put(t.charAt(i),map.getOrDefault(t.charAt(i),0)+1);
+      if(s.length()==0||t.length()==0||s.length()<t.length()){return "";}
+      HashMap<Character,Integer>map=new HashMap<>();
+      Map<Character,Integer>mapt=new HashMap<>();
 
-        }
-        int required=map.size();
-        int r=0,l=0;
-        int create=0;
-        int ans[]={-1,0,0};
-        Map<Character,Integer>maap=new HashMap<>();
-        while(r<s.length()){
-            char c=s.charAt(r);
-            int count=maap.getOrDefault(c,0);
-            maap.put(c,count+1);
-            if(map.containsKey(c)&&maap.get(c).intValue()==map.get(c).intValue()){
-                create++;
-            }
-            while(l<=r&&required==create){
-                c=s.charAt(l);
-                if(ans[0]==-1||ans[0]>r-l+1){
-                    ans[0]=r-l+1;
-                    ans[1]=l;
-                    ans[2]=r;
-                }
-                maap.put(c,maap.get(c)-1);
-                 if(map.containsKey(c)&&maap.get(c).intValue()<map.get(c).intValue()){
-                create--;
-                
-            }
-            l++;
-        }
-        r++;
-        }
-        
-     return ans[0]==-1?"":s.substring(ans[1],ans[2]+1);
+      int l=0,r=0;
+      int [] ans={-1,0,0};
+      int create=0;
+      
+      for(int i=0;i<t.length();i++){
+        map.put(t.charAt(i),map.getOrDefault(t.charAt(i),0)+1);
+      }
+      int required=map.size();
 
+      while(r<s.length()){
+        char c =s.charAt(r);
+        int count=mapt.getOrDefault(c,0);
+        mapt.put(c,count+1);
+        if(map.containsKey(c)&&map.get(c).intValue()==mapt.get(c).intValue()){
+            create++;
+        }
+while(l<=r&&required==create){
+        if(ans[0]==-1||ans[0]>=r-l+1){
+            ans[0]=r-l+1;
+            ans[1]=l;
+            ans[2]=r;
+        }
+       char leftchar=s.charAt(l);
+       mapt.put(leftchar,mapt.get(leftchar)-1);
+      if(map.containsKey(leftchar)&&map.get(leftchar).intValue()>mapt.get(leftchar).intValue()){
+        create--;
+      }
+      l++;
+      }
+      r++;
+      }
+
+    if(ans[0]==-1){
+        return "";
+    }
+
+      
+      return s.substring(ans[1],ans[2]+1);
 
     }
 }
